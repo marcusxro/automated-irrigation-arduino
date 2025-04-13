d76fa0e48f91a9681866896c0df01b0e809fa59f
Marcus Salopaso <114456732+marcusxro@users.noreply.github.com>
2025-04-13 17:36:22 +0800

    added time based watering and led

--- nolcd.cpp
+++ nolcd.cpp
@@ -1,7 +1,27 @@
+#include <Wire.h>
+
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
+
+
 #define PUMP_PIN 4
 #define MOISTURE_SENSOR_PIN A0
 #define WATER_SENSOR_PIN A1
 #define BUZZER_PIN 8
+//led vars
+#define GREEN_LED 13
+#define RED_LED 12
+
+//time vars
+#define DAT_PIN 5
+#define CLK_PIN 6
+
+//button var
+#define BTN_PIN 2
+
+ThreeWire myWire(6, 5, 7);  // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
+
 
 unsigned long previousMillis = 0;
 unsigned long checkInterval = 3000;
@@ -10,21 +30,94 @@ unsigned long countdownStart = 0;
 bool isPumping = false;
 bool isInCountdown = false;
 bool isWaiting = false;
+bool btnState = false;
+
 
 void setup() {
   Serial.begin(9600);
+  pinMode(BTN_PIN, INPUT);  
+
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+
   pinMode(PUMP_PIN, OUTPUT);
   digitalWrite(PUMP_PIN, LOW);
 
   pinMode(BUZZER_PIN, OUTPUT);
-  digitalWrite(BUZZER_PIN, LOW);  
+  digitalWrite(BUZZER_PIN, LOW);
+
+
+
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+
+  Rtc.Begin();
+
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
+
+  if (!Rtc.IsDateTimeValid()) {
+
+    Serial.println("RTC lost confidence in the DateTime!");
+    Rtc.SetDateTime(compiled);
+  }
+
+  if (Rtc.GetIsWriteProtected()) {
+    Serial.println("RTC was write protected, enabling writing now");
+    Rtc.SetIsWriteProtected(false);
+  }
+
+  if (!Rtc.GetIsRunning()) {
+    Serial.println("RTC was not actively running, starting now");
+    Rtc.SetIsRunning(true);
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled) {
+    Serial.println("RTC is older than compile time!  (Updating DateTime)");
+    Rtc.SetDateTime(compiled);
+  } else if (now > compiled) {
+    Serial.println("RTC is newer than compile time. (this is expected)");
+  } else if (now == compiled) {
+    Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+  }
+
+
   Serial.println("System Ready...");
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+  RtcDateTime now = Rtc.GetDateTime();
 
-  if (!isPumping && !isInCountdown && !isWaiting && currentMillis - previousMillis >= checkInterval) {
+  // printDateTime(now);
+  // Serial.println();
+
+  if (!now.IsValid()) {
+    Serial.println("RTC lost confidence in the DateTime!");
+  }
+
+  unsigned long currentMillis = millis();
+  int waterRawVal = analogRead(WATER_SENSOR_PIN);
+
+  if (waterRawVal >= 250) {
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+  } else if (waterRawVal <= 50) {
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    for (int i = 0; i < 3; i++) {  
+      tone(BUZZER_PIN, 1000);      
+      delay(300);                 
+      noTone(BUZZER_PIN);         
+      delay(300);                  
+    }
+
+  } else {
+    noTone(BUZZER_PIN); 
+  }
+
+  if (!isPumping && !isInCountdown && !isWaiting && currentMillis - previousMillis >= checkInterval && waterRawVal >= 50 && isWateringTime(now)) {
     previousMillis = currentMillis;
 
     int value = analogRead(MOISTURE_SENSOR_PIN);
@@ -36,17 +129,14 @@ void loop() {
     Serial.print(moisturePercentage);
     Serial.println("%");
 
-
     int waterRaw = analogRead(WATER_SENSOR_PIN);
     int waterPercentage = map(waterRaw, 300, 700, 0, 100);
-    waterPercentage = constrain(waterPercentage, 0, 100);  
+    waterPercentage = constrain(waterPercentage, 0, 100);
 
     Serial.print("Water Level: ");
     Serial.print(waterRaw);
     Serial.println("%");
-
-
-
+    
     if (value > 800) {
       isPumping = true;
       Serial.println("Pump ON for 3 seconds...");
@@ -55,7 +145,6 @@ void loop() {
       digitalWrite(PUMP_PIN, LOW);
       Serial.println("Pump OFF");
 
-      // Gentle beep pattern 
       for (int i = 0; i < 2; i++) {
         digitalWrite(BUZZER_PIN, HIGH);
         delay(150);
@@ -73,6 +162,11 @@ void loop() {
     }
   }
 
+  if(!isWateringTime(now)) {
+    delay(5000);
+    Serial.println("Not in the time, press the button to set it manually");
+  }
+
   if (isInCountdown) {
     for (int i = 5; i > 0; i--) {
       Serial.print("Waiting ");
@@ -84,4 +178,35 @@ void loop() {
     isWaiting = false;
     Serial.println("Resuming sensor check every 3 seconds...");
   }
+}
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt) {
+  char timestring[10]; 
+
+  snprintf_P(timestring,
+             countof(timestring),
+             PSTR("%02u:%02u:%02u"), 
+             dt.Hour(),
+             dt.Minute(),
+             dt.Second());
+  Serial.print(timestring);
+}
+
+bool isWateringTime(const RtcDateTime& dt) {
+  int hour = dt.Hour();
+  int minute = dt.Minute();
+  
+  // (6:00 AM - 8:59 AM)
+  bool isMorning = (hour == 6 && minute >= 0) || 
+                  (hour == 7) || 
+                  (hour == 8 && minute <= 59);
+  
+  //  (5:00 PM - 7:59 PM)
+  bool isEvening = (hour == 17 && minute >= 0) || 
+                   (hour == 18) || 
+                   (hour == 19 && minute <= 59);
+  
+  return isMorning || isEvening;
 }
