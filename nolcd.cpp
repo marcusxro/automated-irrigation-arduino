ff6b124b89b530c59d20e24ab439dac26e79e3dc
Marcus Salopaso <114456732+marcusxro@users.noreply.github.com>
2025-04-13 09:07:18 +0800

    added buzzer and made it optimized

--- nolcd.cpp
+++ nolcd.cpp
@@ -1,8 +1,10 @@
 #define PUMP_PIN 4
 #define MOISTURE_SENSOR_PIN A0
+#define WATER_SENSOR_PIN A1
+#define BUZZER_PIN 8
 
 unsigned long previousMillis = 0;
-unsigned long checkInterval = 3000;  
+unsigned long checkInterval = 3000;
 unsigned long countdownStart = 0;
 
 bool isPumping = false;
@@ -13,6 +15,9 @@ void setup() {
   Serial.begin(9600);
   pinMode(PUMP_PIN, OUTPUT);
   digitalWrite(PUMP_PIN, LOW);
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW);  
   Serial.println("System Ready...");
 }
 
@@ -31,6 +36,17 @@ void loop() {
     Serial.print(moisturePercentage);
     Serial.println("%");
 
+
+    int waterRaw = analogRead(WATER_SENSOR_PIN);
+    int waterPercentage = map(waterRaw, 300, 700, 0, 100);
+    waterPercentage = constrain(waterPercentage, 0, 100);  
+
+    Serial.print("Water Level: ");
+    Serial.print(waterRaw);
+    Serial.println("%");
+
+
+
     if (value > 800) {
       isPumping = true;
       Serial.println("Pump ON for 3 seconds...");
@@ -38,8 +54,20 @@ void loop() {
       delay(10000);
       digitalWrite(PUMP_PIN, LOW);
       Serial.println("Pump OFF");
-      isPumping = false;
 
+      // Gentle beep pattern 
+      for (int i = 0; i < 2; i++) {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delay(150);
+        digitalWrite(BUZZER_PIN, LOW);
+        delay(200);
+      }
+      delay(300);
+      digitalWrite(BUZZER_PIN, HIGH);
+      delay(250);
+      digitalWrite(BUZZER_PIN, LOW);
+
+      isPumping = false;
       isInCountdown = true;
       countdownStart = millis();
     }
@@ -53,7 +81,7 @@ void loop() {
       delay(1000);
     }
     isInCountdown = false;
-    isWaiting = false; 
+    isWaiting = false;
     Serial.println("Resuming sensor check every 3 seconds...");
   }
 }
